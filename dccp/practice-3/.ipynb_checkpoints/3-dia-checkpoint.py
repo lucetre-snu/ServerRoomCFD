62fa7406f5c10715f00d58e3aeeb248980ff793b
lucetre <lucetre97@gmail.com>
2020-10-20 19:22:14 +0900

    Add practice-6

---
+++ dccp/practice-3/.ipynb_checkpoints/3-dia-checkpoint.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(1, n):
+    for j in range(n-i):
+        print(' ', end='')
+    for j in range(i):
+        print(j+1, end='')
+    for j in range(i-1, 0, -1):
+        print(j, end='')
+    print()
+    
+for i in range(n, 0, -1):
+    for j in range(n-i):
+        print(' ', end='')
+    for j in range(i):
+        print(j+1, end='')
+    for j in range(i-1, 0, -1):
+        print(j, end='')
+    print()
+
