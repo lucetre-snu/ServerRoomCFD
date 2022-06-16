83073455d321bada6a5ca1aeded0ac7b24f317ef
lucetre <lucetre@naver.com>
2022-06-16 16:12:18 +0900

    server room cfd

--- dccp/practice/practice-3/2-sqrt.py
+++
@@ -1,8 +0,0 @@
-a = float(input())
-x = float(input())
-eps = float(input())
-
-while (x*x-a)/a <= -eps or (x*x-a)/a >= eps:
-    print(format(x, '.12f'))
-    x = 1/2 *(x + a/x)
-print(format(x, '.12f'), end='')
